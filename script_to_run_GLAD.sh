08237f321cf3434562cbec3212e5f524551d0ab1
sathe-aditya <sathe_aditya@hotmail.com>
2019-04-11 15:57:56 -0400

    GLAD run bash script

--- script_to_run_GLAD.sh
+++ script_to_run_GLAD.sh
@@ -1,72 +1,92 @@
 #!/bin/bash
 
-echo 'RUN BEGINS'
+echo 'RUN BEGINS 0.1'
 
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_-0.108469535871.txt > ../dilated-cnn-ner/data/phenotype_enhanced/phenotype_enhanced_training_abstract_CoNLL.txt
-
-./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
-
-./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
-
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
-
-echo 'RUN BEGINS'
-
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_-0.108469535871.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.1.txt > ../dilated-cnn-ner/data/phenotype_enhanced/phenotype_enhanced_training_abstract_CoNLL.txt
 
 ./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
 ./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.1.txt
 
-echo 'RUN BEGINS'
+echo 'RUN BEGINS 0.2'
 
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_0.392028417503.txt > phenotype_enhanced_training_abstract_CoNLL.txt
-
-./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
-
-./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
-
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
-
-echo 'RUN BEGINS'
-
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_-0.608967489245.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.2.txt > phenotype_enhanced_training_abstract_CoNLL.txt
 
 ./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
 ./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.2.txt
 
-echo 'RUN BEGINS'
+echo 'RUN BEGINS 0.3'
 
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_0.892526370877.txt > phenotype_enhanced_training_abstract_CoNLL.txt
-
-./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
-
-./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
-
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
-
-echo 'RUN BEGINS'
-
-
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_-1.10946544262.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.3.txt > phenotype_enhanced_training_abstract_CoNLL.txt
 
 ./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
 ./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.3.txt
 
-echo 'RUN BEGINS'
+echo 'RUN BEGINS 0.4'
 
-cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL.txt ../dilated-cnn-ner/data/phenotype_enhanced/hyperparam_m2c_new_labels_-1.60996339599.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.4.txt > phenotype_enhanced_training_abstract_CoNLL.txt
 
 ./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
 ./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
 
-./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations.txt
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.4.txt
+
+echo 'RUN BEGINS 0.5'
+
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.5.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+
+./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.5.txt
+
+echo 'RUN BEGINS 0.6'
+
+
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.6.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+
+./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.6.txt
+
+echo 'RUN BEGINS 0.7'
+
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.7.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+
+./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.7.txt
+
+echo 'RUN BEGINS 0.8'
+
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.8.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+
+./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.8.txt
+
+echo 'RUN BEGINS 0.9'
+
+cat ../dilated-cnn-ner/data/phenotype_enhanced/HPO_training_abstract_CoNLL_shuffled_short.txt ../dilated-cnn-ner/data/phenotype_enhanced/ready_dataset_threshold_0.9.txt > phenotype_enhanced_training_abstract_CoNLL.txt
+
+./bin/preprocess.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/train-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf
+
+./bin/eval-cnn.sh conf/phenotype_enhanced/bilstm-viterbi_multi_gold.conf test models/bilstm-viterbi_phenotype_enhanced_one_rnd_/.tf --print_preds data/phenotype_enhanced/annotations_0.9.txt
